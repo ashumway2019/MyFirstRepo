6d006e3aaabfba8fae52eacd9925ca8b26331619
ashumway2019 <ashumway@gcds.net>
2017-10-10 07:53:09 -0400

    First Commit- MyFirstRepo

---
+++ personality_survey.py
@@ -0,0 +1,9 @@
+print("What is your favorite animal?")
+animal= input()
+
+if animal == "cat":
+    print("Cute animal.")
+elif animal == "dog":
+    print ("That's my favorite, too.")
+else:
+    print (animal + "s sound cool")
